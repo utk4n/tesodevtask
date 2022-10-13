86c910ad82ae8b3e475ba1559329dff56027fdb8
Utkan <utkanocal@gmail.com>
2022-10-13 20:30:02 +0300

    .sort() done.

--- src/components/SortOrder.js
+++ src/components/SortOrder.js
@@ -1,20 +1,26 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nameDesc, nameAsc, dateAsc, dateDesc } from "../redux/newRecordSlice";
 const SortOrder = ({ isOpen }) => {
+  const dispatch = useDispatch();
   if (!isOpen) return false;
-
   return (
     <ul className="sort_order_list">
-      {[
-        "Name ascending",
-        "Name descending",
-        "Year ascending",
-        "Year descending",
-      ].map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+      <li onClick={() => dispatch(nameAsc())}>Name ascending</li>
+      <li onClick={() => dispatch(nameDesc())}>Name descending</li>
+      <li onClick={() => dispatch(dateAsc())}>Year ascending</li>
+      <li onClick={() => dispatch(dateDesc())}>Year descending</li>
     </ul>
   );
 };
 
 export default SortOrder;
+
+// {[
+//   "Name ascending",
+//   "Name descending",
+//   "Year ascending",
+//   "Year descending",
+// ].map((item, index) => (
+//   <li onClick={func} key={index}>{item}</li>
+// ))}
